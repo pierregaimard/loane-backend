1da6d86ae0ff500e1994a0af08fe06260a36d43c
Pierre Gaimard <pierre@gaimard-family.fr>
2021-12-13 10:21:55 +0100

    Add user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  findBy(options: object): Promise<User[]> {
+    return this.userRepository.find(options);
+  }
+
+  findOne(id: string): Promise<User> {
+    return this.userRepository.findOne(id);
+  }
+
+  findOneBy(options: object): Promise<User> {
+    return this.userRepository.findOne(options);
+  }
+}
