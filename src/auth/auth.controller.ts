1d4bc564414dc287cfdd0101d16ca97dc9250279
Pierre Gaimard <pierre@gaimard-family.fr>
2021-12-13 15:14:44 +0100

    Implements authService login method into auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Request, Post, UseGuards, HttpCode } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @Post('login')
   @UseGuards(LocalAuthGuard)
   @HttpCode(200)
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
