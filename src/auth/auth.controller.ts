529e9374c422885e4ad9348da0f017e6b002af55
Pierre Gaimard <pierre@gaimard-family.fr>
2021-12-13 15:46:48 +0100

    Add Public decorator to auth/login entrypoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Request, Post, UseGuards, HttpCode } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
+import { Public } from './auth.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
+  @Public()
   @UseGuards(LocalAuthGuard)
   @HttpCode(200)
   async login(@Request() req) {
