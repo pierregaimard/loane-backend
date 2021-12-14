a72d4bb58cc75749946fb800779f6de9a13d8741
Pierre Gaimard <pierre@gaimard-family.fr>
2021-12-14 20:35:39 +0100

    Add UserController

---
+++ src/user/user.controller.ts
@@ -0,0 +1,44 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserCreateDto } from './dto/user.create.dto';
+import { PasswordHelper } from '../security/password.helper';
+
+@Controller('users')
+export class UserController {
+  constructor(
+    private userService: UserService,
+    private passwordHelper: PasswordHelper,
+  ) {}
+
+  @Post()
+  async create(@Body() user: UserCreateDto) {
+    user.password = await this.passwordHelper.getHash(user.password);
+
+    try {
+      return await this.userService.create(user);
+    } catch (err) {
+      throw new BadRequestException(err.sqlMessage);
+    }
+  }
+
+  @Get(':id')
+  async get(@Param('id') id: string) {
+    const user = await this.userService.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    const { password, ...data } = user;
+
+    return data;
+  }
+}
