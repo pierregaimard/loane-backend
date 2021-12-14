ed3f3cb638718f48bca8fd4a2f3e69e626d143ac
Pierre Gaimard <pierre@gaimard-family.fr>
2021-12-14 20:36:48 +0100

    Add dependencies to UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { SecurityModule } from '../security/security.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), SecurityModule],
   providers: [UserService],
+  controllers: [UserController],
   exports: [UserService],
 })
 export class UserModule {}
