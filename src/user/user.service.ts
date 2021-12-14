878996d4ba684ab3d0177ee8a6f6a6d5e6dc061b
Pierre Gaimard <pierre@gaimard-family.fr>
2021-12-14 20:35:21 +0100

    Add create method to user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
-import { Repository } from 'typeorm';
+import { InsertResult, Repository } from "typeorm";
+import { UserCreateDto } from "./dto/user.create.dto";
 
 @Injectable()
 export class UserService {
@@ -24,5 +25,9 @@ export class UserService {
 
   findOneBy(options: object): Promise<User> {
     return this.userRepository.findOne(options);
+  }
+
+  create(user: UserCreateDto): Promise<User> {
+    return this.userRepository.save(user);
   }
 }
