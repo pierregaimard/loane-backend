182efdf91d22d2551f2faa686bacf167f08e48f2
Pierre Gaimard <pierre@gaimard-family.fr>
2021-12-29 09:33:23 +0100

    Set attributes as optional in User entity

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,25 +1,50 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+} from 'class-validator';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id: number;
+  id?: number;
 
   @Column({ type: 'varchar', length: 60, nullable: false, unique: true })
-  email: string;
+  @IsEmail()
+  @IsNotEmpty()
+  email?: string;
 
   @Column({ type: 'varchar', length: 60, nullable: false, unique: true })
-  username: string;
+  @IsNotEmpty()
+  @Length(3, 60)
+  @Matches(/^[a-zA-Z][a-zA-Z0-9]{2,}$/, {
+    message: '$property Should only contain letters and numbers',
+  })
+  username?: string;
 
   @Column({ type: 'varchar', nullable: false })
-  password: string;
+  @IsNotEmpty()
+  @Length(3, 60)
+  @IsString()
+  password?: string;
 
   @Column({ type: 'varchar', length: 60, nullable: false })
-  firstName: string;
+  @IsNotEmpty()
+  @Matches(/^[a-zA-Z]+(-?[a-zA-Z])*$/, {
+    message: '$property Should only contain letters and dashes',
+  })
+  firstName?: string;
 
   @Column({ type: 'varchar', length: 60, nullable: false })
-  lastName: string;
+  @IsNotEmpty()
+  @Matches(/^[a-zA-Z]+(-?[a-zA-Z])*$/, {
+    message: '$property Should only contain letters and dashes',
+  })
+  lastName?: string;
 
   @Column({ type: 'boolean', default: true })
-  isActive: boolean;
+  isActive?: boolean;
 }
